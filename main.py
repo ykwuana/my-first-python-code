061e6c2d9ddd3dfa0fa76741e59e5733ce31b9b5
ykwuana <ykwuana@gmail.com>
2024-01-17 15:54:26 +0700

    my first python code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 """"
-ini adalah demo project pertama dengan python
+semua sintaksis dasar bahasa pemrograman terdiri dari :
+
+1. sekuensial : langkah berurutan
+2. percabangan : langkah melompat jika kondisi terpenuhi
+3. perulangan : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
 
 print("Hello world")
